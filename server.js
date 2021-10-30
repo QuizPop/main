c82a5f56b35571b2d80d9d9b59e4871c11ead685
tylerrimkunas <tyler.rimkunas@stonybrook.edu>
2021-10-30 11:25:48 -0400

    added heroku post build

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
-
+const path = require('path');
 const app = express();
 
 // Bodyparser middleware
@@ -36,6 +36,16 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+// serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
