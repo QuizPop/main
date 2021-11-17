3914687c75211c498ea9cde7ad3f2564415ba182
Zain Makda <zainmakda@yahoo.com>
2021-11-17 18:48:54 -0500

    can print out platform info

--- client/src/components/platform/Platform-List.js
+++ client/src/components/platform/Platform-List.js
@@ -1,37 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 
-export class platfromList extends Component {
-constaractor(state) {
-
-  state = {
-  Platfroms:[]
-  };
+export default class platformList extends Component {
+constructor(props) {
+super(props);
+this.state = {
+Users: []
+};
 }
 
-  componentDidMount() {
-    var x =[]
-    axios.get('/api/Platforms/')
-    .then(({ data}) =>(console.log(data))) // <-- set state
-    .catch(e => console.log(e))
-  }
-  render() {
 
-    // const cities=this.state.locations.map(location => (
-    //   <div key={location._id}>
-    //  <h1>{location.name}</h1>
-    //   <p>{location.description}</p> 
-    //     <h1>platforms</h1>
-    //   </div>
-    // ));
-    
-    return (
-      <div className="Cities">
-        {/* {cities} */}
-      </div>
-    );
-  }
+getUsersData() {
+axios
+.get(`/api/Platforms`, {})
+.then(res => {
+const data = res.data
+console.log(data)
+const users = data.map(u =>
+  <div>
+  <p>{u.name}</p>
+  <p>{u.owner_ID}</p>
+  <p>{u.description}</p>
+  <p>{u.tags}</p>
+  </div>
+  )
+
+  this.setState({
+      users
+  })
+
+})
+.catch((error) => {
+  console.log(error)
+})
+
+}
+componentDidMount(){
+this.getUsersData()
 }
 
-export default platfromList
+render() {
+
+  return (
+    <div>
+      {this.state.users}
+    </div>
+    )
+  }
+}
